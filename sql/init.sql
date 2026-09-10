e69f279c817af07be502d6ab83d53c4dc28165a1
Tanya <cheerfulness26@gmail.com>
2026-09-10 21:12:17 +0300

    New update for sql init

--- sql/init.sql
+++ sql/init.sql
@@ -1,10 +1,11 @@
-BEGIN;
+
+BEGIN; 
 
 -- ==========================================
--- 1. Creating products dataset with its parameters
+-- 1. Creating products table with their parameters
 -- ==========================================
 
-CREATE TABLE IF NOT EXISTS olist_products_dataset (
+CREATE TABLE IF NOT EXISTS olist_products (
     product_id VARCHAR(50) PRIMARY KEY,
     product_category_name VARCHAR(50),
     product_name_length INT,
@@ -14,6 +15,32 @@ CREATE TABLE IF NOT EXISTS olist_products_dataset (
     product_length_cm INT,
     product_height_cm INT,
 	product_width_cm INT
+);
+
+-- ==========================================
+-- 2. Creating table with order payments information
+-- ==========================================
+
+CREATE TABLE IF NOT EXISTS olist_order_payments (
+    order_id VARCHAR(50) PRIMARY KEY,
+    payment_sequential INT,
+    payment_type VARCHAR(50),
+    payment_installments INT,
+    payment_value FLOAT(24)
+);
+
+-- ==========================================
+-- 3. Creating review table 
+-- ==========================================
+
+CREATE TABLE IF NOT EXISTS olist_order_reviews (
+    review_id VARCHAR(50) PRIMARY KEY, -- Uniquely identifies each review in this table
+    order_id VARCHAR(50) NOT NULL,    -- Identifies which order this review belongs to
+    review_score SMALLINT,
+    review_comment_title TEXT,
+    review_comment_message TEXT,
+    review_creation_date DATE,
+    review_answer_timestamp TIMESTAMPTZ
 );
 
 COMMIT;
