83c57620c499f7941f65620078b07e52c21f57d8
Tanya <cheerfulness26@gmail.com>
2026-09-22 21:19:06 +0300

    work with time period and overdue days from order datasets

--- scripts/init.py
+++ scripts/init.py
@@ -1,15 +1,28 @@
+
+##import libraries
 import os
-os.environ['PGCLIENTENCODING'] = 'utf-8'
-
-from config import *
-
+from datetime import datetime
 import pandas as pd
 from sqlalchemy import create_engine
 
+## import local server set up info
+from config import *
+
+## Take month from env variable
+TARGET_DATE = os.getenv("TARGET_DATE", '2018-08-06')
+
+## Define engine for working with local postgres server
 engine = create_engine(db_url)
 
-df = pd.read_sql_query("SELECT * FROM olist_sellers LIMIT 10;", con=engine)
-print(df.head())
+## Read datasets for target period
+olist_orders = pd.read_sql_query(f"SELECT * FROM olist_orders\
+                                 WHERE DATE(order_estimated_delivery_date) = '{TARGET_DATE}'; ", con=engine)
 
+## Create dataframe with delayed orders
+olist_overdue_orders = olist_orders[olist_orders['order_estimated_delivery_date'] < olist_orders['order_delivered_customer_date']]
+
+olist_overdue_orders['amount_overdue_days'] = olist_orders['order_delivered_customer_date'] - olist_orders['order_estimated_delivery_date']
+
+print(olist_overdue_orders['amount_overdue_days'])
 
 
